781b99b2106554df90c45539ce7f98fe4e93729d
arthas <arthastheking113@gmail.com>
2021-02-22 00:51:27 -0600

    update profile page

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,20 +1,20 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-function display_c() {
-    var refresh = 1000; // Refresh rate in milli seconds
-    mytime = setTimeout('display_ct()', refresh)
-}
+//function display_c() {
+//    var refresh = 1000; // Refresh rate in milli seconds
+//    mytime = setTimeout('display_ct()', refresh)
+//}
 
-function display_ct() {
-    var x = new Date()
-    let options = {
-        hour: "2-digit", minute: "2-digit",
-        second: "2-digit", weekday: "long", year: "numeric", month: "short",
-        day: "numeric"
-    };  
-    document.getElementById('ct').innerHTML = x.toLocaleTimeString("en-us", options);
-    display_c();
-}
+//function display_ct() {
+//    var x = new Date()
+//    let options = {
+//        hour: "2-digit", minute: "2-digit",
+//        second: "2-digit", weekday: "long", year: "numeric", month: "short",
+//        day: "numeric"
+//    };  
+//    document.getElementById('ct').innerHTML = x.toLocaleTimeString("en-us", options);
+//    display_c();
+//}
 
 $('#contact').summernote({
     height: 300,
