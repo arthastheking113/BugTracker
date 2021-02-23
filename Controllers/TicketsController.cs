7f597bc6d25afb777696e32381fcbbf4169c57b3
arthas <arthastheking113@gmail.com>
2021-02-23 17:27:34 -0600

    update edit ticket

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -137,9 +137,9 @@ namespace BugTracker.Controllers
                     _context.Update(ticket);
                     await _context.SaveChangesAsync();
 
-                    var userId = _userManager.GetUserId(User);
-                    Ticket newTicket = await _context.Ticket.Include(t => t.TicketType).Include(t => t.Status).Include(t => t.Priority).Include(t => t.Developer).AsNoTracking().FirstOrDefaultAsync(t => t.Id == id);
-                    await _customHistoryService.AddHistoryAsync(oldTicket, newTicket, userId);
+                    //var userId = _userManager.GetUserId(User);
+                    //Ticket newTicket = await _context.Ticket.Include(t => t.TicketType).Include(t => t.Status).Include(t => t.Priority).Include(t => t.Developer).AsNoTracking().FirstOrDefaultAsync(t => t.Id == id);
+                    //await _customHistoryService.AddHistoryAsync(oldTicket, newTicket, userId);
 
                 }
                 catch (DbUpdateConcurrencyException)
